e0f09bfed3c595dcebe45ef9956d55760c80eff3
Daniel Cunha <dcunha@tomitribe.com>
2016-02-28 08:24:57 -0800

    Refactore class

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,25 +8,36 @@ import java.util.concurrent.Executors;
 
 public class Client {
     private final Executor executorService = Executors.newCachedThreadPool();
+    private final Socket socket;
+    private final PrintWriter out;
 
-    public static void main(String[] args) {
-        new Client().createClient("localhost", 8080);
+    public Client(String host, int port, boolean console) throws IOException {
+        this.socket = new Socket(host, port);
+        this.executorService.execute(new ClientMsg(socket));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        if (console) {
+            createBuffered();
+        }
     }
 
-    public void createClient(String hots, int port) {
-        try (Socket socket = new Socket(hots, port)) {
-            executorService.execute(new ClientMsg(socket));
+    public void createBuffered() throws IOException {
+        final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        final PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        while (true) {
+            String input = bf.readLine();
+            sendMsg(input);
+        }
+    }
 
-            final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-            final PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            while (true) {
-                String input = bf.readLine();
-                if (input != null) {
-                    out.println(input);
-                }
-            }
+    public void sendMsg(String msg) throws IOException {
+        out.println(msg);
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Client("localhost", 8080, true);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Erro when trying connect: " + e.getMessage());
         }
     }
 }
